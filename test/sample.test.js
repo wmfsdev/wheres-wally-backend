61fde3a567ce7f2b8520516c6fad8424197fda26
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-12-22 22:46:29 +0000

    import deep mock for prisma client, mock prisma module, create sample test for mock

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,4 +1,15 @@
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
+import { createUser } from '../controllers/indexController'
+import prisma from '../libs/__mocks__/prisma'
+
+vi.mock('../libs/prisma')
+
+test('createUser should return the generated user', async () => {
+  const newUser = { email: 'user@prisma.io', name: 'Prisma Fan' }
+  prisma.user.create.mockResolvedValue({ ...newUser, id: 1 })
+  const user = await createUser(newUser)
+  expect(user).toStrictEqual({ ...newUser, id: 1 })
+})
 
 // sample test
 test('1 === 1', () => {
