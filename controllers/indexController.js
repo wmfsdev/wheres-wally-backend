de401d50c7a5aed4110ee36c7367717787cac356
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-12-22 22:47:40 +0000

    create sample test function for prisma client mock

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,14 @@
 
+import prisma from "../libs/prisma";
+
+async function createUser(user) {
+    return await prisma.user.create({
+        data: user,
+    }); 
+}
+
 function sampleController(req, res) {
     res.send('Hello World!')
 }
 
-module.exports = { sampleController }
+module.exports = { sampleController, createUser }
