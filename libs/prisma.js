3b0497fbd7dcd67a2c718a943f31a9896f9224e2
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-12-27 22:53:19 +0000

    add dynamically generated prisma client based on user environment

--- libs/prisma.js
+++ libs/prisma.js
@@ -1,4 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const databaseUrl = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
+
 export default prisma
