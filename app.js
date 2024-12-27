765d9a59571127a41bda140307d1532a4adb512a
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-12-27 22:50:58 +0000

    add export for express app (abstract out app for testing and non-testing development purposes)

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 const indexRouter = require("./routes/indexRouter")
 
 // sample route
 app.use("/", indexRouter)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app
