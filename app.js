d10a3d899790944e804406ce05fdb7cf88f5cad0
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-12-29 22:14:14 +0000

    add json and urlencoded middleware

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import indexRouter from './routes/indexRouter.js'
 
 const app = express()
 
-import indexRouter from './routes/indexRouter.js'
-
 // sample route
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter)
 
 export default app
