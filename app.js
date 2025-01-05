65d91638e3cc382d2629623bd7d40e9191b6c7ad
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-01-05 20:23:57 +0000

    remove express-session as application-level middleware

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import indexRouter from './routes/indexRouter.js'
-import expressSession from 'express-session'
-import { PrismaSessionStore } from '@quixo3/prisma-session-store'
-import prisma from './libs/prisma.js'
+import gameRouter from './routes/gameRouter.js'
 
 const app = express()
 
@@ -10,24 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-app.use(expressSession({
-    cookie: {
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    },
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: new PrismaSessionStore(
-        prisma,
-        {
-           checkPeriod: 2 * 60 * 1000,
-           dbRecordIdIsSessionId: true,
-           dbRecordIdFunction: undefined,
-        }
-    )
-})
-)
-
 app.use("/", indexRouter)
+app.use("/game", gameRouter)
 
 export default app
