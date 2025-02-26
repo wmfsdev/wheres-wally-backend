4de1423a407f2087d2a949b272c1325a9dd3f4be
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-26 21:56:47 +0000

    create functions for updating database with game runtime for player as well as players chosen name (frontend input)

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,5 +1,5 @@
 
-import { getCoordinateMatchStatus, checkPlayerStatus, createPlayerConnectSession, getImageCoordinates, incrementFoundCoordinates, extractSid } from "../utils/helper.js";
+import { getCoordinateMatchStatus, checkPlayerStatus, createPlayerConnectSession, getImageCoordinates, incrementFoundCoordinates, extractSid, calculateGameRuntime } from "../utils/helper.js";
 import prisma from "../libs/prisma.js";
 
 
@@ -7,22 +7,12 @@ async function post_check_coordinates(req, res, next) {
   console.log("check coordinates")
   const imageId = req.params.id
   const characterNameId = req.body.character
-  console.log("coordinates", req.body.coordinates )
-
   const { x, y } = req.body.coordinates
-
-  // console.log("cookies: ", req.cookies)
   const sid = extractSid(req.cookies['connect.sid'])
   
-  // console.log("SID", sid)
-  // 0. check to see if there is a player with session ID
   const playerExists = await checkPlayerStatus(sid)
-  // console.log("playerExists", playerExists)
 
   if (!playerExists) {
-    console.log("!playerExists")
-   // const sessionId = sid
-    // no player found - create and connect to session
     createPlayerConnectSession(sid)
   } // otherwise there is a player and carry on...
   
@@ -39,25 +29,75 @@ async function post_check_coordinates(req, res, next) {
     // increment player's FoundCoordinates counter +1
     const foundCoordinateCount = await incrementFoundCoordinates(sid)
     // WIN CHECK
-    if (foundCoordinateCount === 5) {
-      return res.status(200).json({ status: "win!" })
+    if (foundCoordinateCount === 2) {
+      console.log("WIN")
+      const gameRuntime = await calculateGameRuntime(sid)
+      return res.status(200).json({ status: "win!", gameRuntime: gameRuntime, playerId: sid  })
     }
-    return res.status(200).json({ status: "found element" })
-    
-    
+    return res.status(200).json({ status: "found element", gameRuntime: null})
   } else {
     console.log("didn't match:(")
-    
-    return res.status(200).json({ status: "no element found" })
+    return res.status(200).json({ status: "no element found", gameRuntime: null })
   }
-  
 }
 
 async function getImage(req, res, next) {
   const { storedXCoordinate, storedYCoordinate } = await getImageCoordinates()
-  console.log("getImage destructure", storedXCoordinate, storedYCoordinate)
   res.status(200).json()
 }
 
-export { post_check_coordinates, getImage }
-// connectPlayerSessionRelation, createCoordinates, test
+
+async function put_player_gametime(req, res, next) {
+  
+  const { playerId, gameRuntime } = req.body
+
+  const response = await prisma.player.update({
+    where: {
+      sessionId: playerId
+    },
+    data: {
+      gameLength: gameRuntime
+    }
+  })
+}
+
+async function put_player_name(req, res, next) {
+  console.log("put_player_name")
+  const { playerId, gameRuntime, playerName, boardId } = req.body
+
+  if (gameRuntime) {
+    await prisma.player.update({
+      where: {
+        sessionId: playerId
+      },
+      data: {
+        name: playerName,
+        gameLength: gameRuntime,
+        board: boardId
+      }
+    })
+    res.status(200).json()
+    return
+  } else {
+    const [one, two] = await prisma.$transaction([
+      prisma.player.update({
+        where: {
+          sessionId: playerId
+        },
+        data: {
+          name: playerName,
+          gameLength: gameRuntime,
+          board: boardId
+        }
+      }),
+      prisma.session.delete({
+        where: {
+          sid: playerId
+        }
+      })
+    ])
+    res.status(200).json()
+  }
+}
+
+export { post_check_coordinates, getImage, put_player_gametime, put_player_name }
