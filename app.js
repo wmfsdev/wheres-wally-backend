b5a1d82f23f4949ba57fbd557776ff97b55a73a1
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-11 21:12:21 +0000

    add origin value

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: /vercel\.app$/,
+    origin: 'https://wheres-wally-frontend-xi.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
