44f189dc86e4a68314b228b9ff63227b130b3b69
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-11 21:31:46 +0000

    **

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, // 'wheres-wally-frontend-xi.vercel.app',
+    origin: 'wheres-wally-frontend-xi.vercel.app',
     allowedHeaders: ['Content-Type', 'Authorization','Connection'],
     exposedHeaders: ['application/x-www-form-urlencoded', 'multipart/form-data', 'text/plain'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
