13139f60b4afa932ce8be166c71f0f69c9539f89
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-14 20:58:50 +0000

    **

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Private-Network", true);
