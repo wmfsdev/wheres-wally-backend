0b182d00bafdb88232a3d0cfb523ecf5b19d3d9d
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-14 19:42:18 +0000

    **

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.set('trust proxy', 1)
 app.use(cors({
     origin: 'https://wheres-wally-frontend-xi.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Access-Control-Allow-Origin',
-    exposedHeaders: 'Access-Control-Allow-Origin',
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization', 'X-Content-Type-Options', 'Accept', 'X-Requested-With', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Methods'],
+    exposedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization', 'X-Content-Type-Options', 'Accept', 'X-Requested-With', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Methods'],
     preflightContinue: true,
     optionsSuccessStatus: 204,
     credentials: true,
