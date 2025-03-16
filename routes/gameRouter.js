19f9f99984fe361c2e95086a0e6b078ab35d64b1
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-16 20:40:18 +0000

    revert testing

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -27,9 +27,7 @@ const createSession = expressSession({
 
 const gameRouter = Router()
 
-// gameRouter.use('/board', conditionalCookieDestroy, createSession)
-
-gameRouter.get('/board/:id', createSession)
+gameRouter.use('/board', conditionalCookieDestroy, createSession)
 
 gameRouter.put('/player', put_player_name)
 
