b167420ee8074a278e2dbdc33b767136e92e4d03
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-19 20:53:17 +0000

    cleanup

--- app.js
+++ app.js
@@ -17,25 +17,6 @@ app.use(cors({
     credentials: true,
 }))
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://wheres-wally-frontend-xi.vercel.app"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Private-Network", true);
-//   res.setHeader("Access-Control-Max-Age", 7200);
-//   next();
-// });
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
