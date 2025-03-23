f6cbea879397c9edeb7ad51ccb536840cc178aaf
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-23 23:02:21 +0000

    add  for multiple player queries distinguished by their respective winning board

--- controllers/leaderBoardController.js
+++ controllers/leaderBoardController.js
@@ -2,21 +2,54 @@
 import prisma from "../libs/prisma.js";
 
 async function get_times(req, res, next) {
-  const response = await prisma.player.findMany({
-    take: 10,
-    where: {
-      gameLength: { not: null }
-    },
-    select: {
-      name: true,
-      gameLength: true,
-      board: true,
-    },
-    orderBy: {
-      gameLength: 'asc'
-    }
-  })
-  res.status(200).json(response)
+  const [ maze, football, rome ] = await prisma.$transaction([
+    prisma.player.findMany({
+      take: 10,
+      where: {
+        board: 'maze',
+        gameLength: { not: null }
+      },
+      select: {
+        name: true,
+        gameLength: true,
+        board: true,
+      },
+      orderBy: {
+        gameLength: 'asc'
+      }
+    }),
+    prisma.player.findMany({
+      take: 10,
+      where: {
+        board: 'football',
+        gameLength: { not: null }
+      },
+      select: {
+        name: true,
+        gameLength: true,
+        board: true,
+      },
+      orderBy: {
+        gameLength: 'asc'
+      }
+    }),
+    prisma.player.findMany({
+      take: 10,
+      where: {
+        board: 'rome',
+        gameLength: { not: null }
+      },
+      select: {
+        name: true,
+        gameLength: true,
+        board: true,
+      },
+      orderBy: {
+        gameLength: 'asc'
+      }
+    })
+  ])
+  res.status(200).json({maze, football, rome}) 
 }
 
 export { get_times }
